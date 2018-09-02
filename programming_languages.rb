24fc398ad1cec65efb2b70ed063a2ec4513f2449
katy@karstaway.com <katy@karstaway.com>
2018-09-02 13:34:15 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,8 @@ def reformat_languages(languages)
   languages.each do |type, info_hash|
     type.each do |name, attributes|
       new_hash[name] ||=attributes
-      new_hash[name][:style] ||= []
-      new_hash[name][:style] << type
+      new_hash[name]["style"] ||= []
+      new_hash[name]["style"] << type
     end
   end
   new_hash
