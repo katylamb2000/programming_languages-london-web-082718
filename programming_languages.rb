d0f615dad38721bb95c6f3791ab52d28826caf14
katy@karstaway.com <katy@karstaway.com>
2018-09-02 13:36:29 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,24 +1,24 @@
-def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |type, name|
-    type.each do |name, attributes|
-      new_hash[name] ||=attributes
-      new_hash[name]["style"] ||= []
-      new_hash[name]["style"] << type
-    end
-  end
-  new_hash
-end
-
-
 # def reformat_languages(languages)
-#   new_languages = {}
-#   languages.each do |type, hash|
-#     hash.each do |name, attributes|
-#       new_languages[name] ||= attributes
-#       new_languages[name][:style] ||= []
-#       new_languages[name][:style] << type
+#   new_hash = {}
+#   languages.each do |type, name|
+#     type.each do |name, attributes|
+#       new_hash[name] ||=attributes
+#       new_hash[name]["style"] ||= []
+#       new_hash[name]["style"] << type
 #     end
 #   end
-#   new_languages
+#   new_hash
 # end
+
+
+def reformat_languages(languages)
+  new_languages = {}
+  languages.each do |type, hash|
+    hash.each do |name, attributes|
+      new_languages[name] ||= attributes
+      new_languages[name][:style] ||= []
+      new_languages[name][:style] << type
+    end
+  end
+  new_languages
+end
